7bdab4b42c4d15ba0bbaf0cf9ddd0bee6eee56da
Gauthier Lepour <gauthier.lepour@gmail.com>
2024-02-23 16:19:04 +0100

    hello

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+films = URI.open(url).read
+parsed_films = JSON.parse(films)
+
+p parsed_films["results"].each do |movie|
+  Movie.create(
+    title: movie[:title],
+    overview: movie[:overview],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie[:poster_path]}",
+    rating: movie[:vote_average]
+  )
+end
